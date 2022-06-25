3a10c5e3df6a13fa95441638a051a884d1aec4ea
Omer Faruk Kanli <kanlomerfaruk@gmail.com>
2022-06-25 12:20:52 +0300

    .

--- todo.js
+++ todo.js
@@ -1,26 +1,108 @@
 const form = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo");
 const todoList = document.querySelector(".list-group");
-const FirstCardBody = document.querySelector(".card-body")[0];
-const SecondCardBody = document.querySelector(".card-body")[1];
+const FirstCardBody = document.querySelectorAll(".card-body")[0];
+const SecondCardBody = document.querySelectorAll(".card-body")[1];
 const filter = document.querySelector("#filter");
 const clearButton = document.querySelector("#clear-todos");
 
 eventListeners();
+
 function eventListeners() {
     form.addEventListener("submit", addtodo);
+    document.addEventListener("DOMContentLoaded", loadTodosToUI);
+    SecondCardBody.addEventListener("click", deleteTodo);
+    filter.addEventListener("keyup", filterTodos);
+    clearButton.addEventListener("click",clearAllTodos);
+}
+
+function clearAllTodos(e){
+    if (confirm("Tümünü silmek ister misiniz ?")){
+        // todoList.innerHTML ="";//yavaş
+
+        while(todoList.firstElementChild != null){
+            todoList.removeChild(todoList.firstElementChild);
+        }
+        localStorage.removeItem("todos");
+
+    }
 
 }
 
+
+function filterTodos(e) {
+    const filterValue = e.target.value.toLowerCase();
+    const ListItems = document.querySelectorAll(".list-group-item");
+
+    ListItems.forEach(function (listItem) {
+        const text = listItem.textContent.toLowerCase();
+        if (text.indexOf(filterValue) === -1) {
+            listItem.setAttribute("style" ,"display: none !important");
+        }
+        else {
+            listItem.setAttribute("style" ,"display: block");
+        }
+
+    });
+}
+
+function deleteTodo(e) {
+    if (e.target.className === "fa fa-remove") {
+        e.target.parentElement.parentElement.remove();
+        deleteTodoFromStorage(e.target.parentElement.parentElement.textContent);
+        showAlert("success", "Başarıyla silindi");
+    }
+};
+
+function deleteTodoFromStorage(deletetodo) {
+    let todos = getTodosFromStorage();
+
+    todos.forEach(function (todo, index) {
+        if (todo === deletetodo) {
+            todos.splice(index, 1);
+        }
+    });
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function loadTodosToUI() {
+    let todos = getTodosFromStorage();
+
+    todos.forEach(function (todo) {
+        addTodoToUI(todo);
+    });
+}
+
 function addtodo(e) {
-    const newTodo = todoInput.value.trim();
+    const newTodo = todoInput.value;
     if (newTodo === "") {
         showAlert("danger", "Lütfen bir todo giriniz");
     }
     else {
         addTodoToUI(newTodo);
+        addTodoToStorage(newTodo);
+        showAlert("success", "Başarıyla Eklendi");
     }
     e.preventDefault();
+
+}
+function getTodosFromStorage() {
+    let todos;
+
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    }
+    else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
+    return todos;
+}
+
+function addTodoToStorage(newTodo) {
+    let todos = getTodosFromStorage();
+    todos.push(newTodo);
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
 
 function showAlert(type, message) {
@@ -28,16 +110,20 @@ function showAlert(type, message) {
     alert.className = `alert alert-${type}`;
     alert.textContent = message;
     FirstCardBody.appendChild(alert);
+
+    setTimeout(function () {
+        alert.remove();
+    }, 2000);
 }
 
-function addTodoToUI(newTodo) {
+function addTodoToUI(params) {
     const listItem = document.createElement("li");
     const link = document.createElement("a");
     link.href = "#";
     link.className = "delete-item";
-    link.innerHTML = "<i class='fa fa-remove'></i>"
+    link.innerHTML = "<i class = 'fa fa-remove'></i>";
     listItem.className = "list-group-item d-flex justify-content-between";
-    listItem.appendChild(document.createTextNode(newTodo));
+    listItem.appendChild(document.createTextNode(params));
     listItem.appendChild(link);
     todoList.appendChild(listItem);
     todoInput.value = "";
